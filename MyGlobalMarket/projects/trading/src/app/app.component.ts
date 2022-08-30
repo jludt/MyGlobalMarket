4525e8b46d351871684ccb266fa57790e64da0a8
JLudt <jludt11@gmail.com>
2022-08-30 14:30:49 -0400

    got tradingviewwidget module working

--- MyGlobalMarket/projects/trading/src/app/app.component.ts
+++ MyGlobalMarket/projects/trading/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ITradingViewWidget, Themes } from 'tradingview-widget'
+
 
 @Component({
   selector: 'app-root',
@@ -7,20 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public forecasts?: WeatherForecast[];
+  widgetConfig: ITradingViewWidget = {
+    symbol: 'MSFT',
+    widgetType: 'widget',
+    allow_symbol_change: true,
+    height: 540,
+    width: 980,
+    hideideas: true,
+    hide_legend: false,
+    hide_side_toolbar: true,
+    hide_top_toolbar: false,
+    theme: Themes.LIGHT,
+  };
 
-  constructor(http: HttpClient) {
-    http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
+  constructor() {
+
   }
 
   title = 'MyGlobalMarket';
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
--- MyGlobalMarket/projects/trading/src/app/app.module.ts
+++ MyGlobalMarket/projects/trading/src/app/app.module.ts
@@ -3,13 +3,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TradingviewWidgetModule } from 'tradingview-widget'
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatSelectModule } from '@angular/material/select'
+import { MatProgressBarModule } from '@angular/material/progress-bar'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, TradingviewWidgetModule, MatCardModule,
+    MatChipsModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
